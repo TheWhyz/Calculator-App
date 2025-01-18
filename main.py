e110ab03fa87071142639b39a2ab9622c12bd7c3
Jaydan <janiesco63@gmail.com>
2025-01-17 18:04:44 -0800

    Designed the layout of the calculator

---
+++ main.py
@@ -0,0 +1,45 @@
+from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit, QPushButton, QHBoxLayout, QVBoxLayout, QGridLayout
+
+app = QApplication([])
+main_window = QWidget()
+main_window.setWindowTitle("Calculator App")
+main_window.resize(250, 300)
+
+text_box = QLineEdit()
+grid = QGridLayout()
+
+buttons = [
+    "7", "8", "9", "/",
+    "4", "5", "6", "*",
+    "1", "2", "3", "-",
+    "0", ".", "=", "+"
+]
+
+clear = QPushButton("Clear")
+delete = QPushButton("<")
+
+row = 0
+col = 0
+for text in buttons:
+    button = QPushButton(text)
+    # button.clicked.connect(None)
+    grid.addWidget(button, row, col)
+    col += 1
+
+    if col > 3:
+        col = 0
+        row += 1
+
+master_layout = QVBoxLayout()
+master_layout.addWidget(text_box)
+master_layout.addLayout(grid)
+
+button_row = QHBoxLayout()
+button_row.addWidget(clear)
+button_row.addWidget(delete)
+master_layout.addLayout(button_row)
+
+main_window.setLayout(master_layout)
+
+main_window.show()
+app.exec_()
